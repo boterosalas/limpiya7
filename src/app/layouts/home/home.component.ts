58f2ca32df878a9cd7ef05be88f17fa5866bcd10
boterosalas <boterosalas@gmail.com>
2025-01-21 15:01:12 -0500

    Feature: Se crea pipeline para despliegue

--- src/app/layouts/home/home.component.ts
+++ src/app/layouts/home/home.component.ts
@@ -13,19 +13,19 @@ export class HomeComponent {
 
   services = [
     {
-      image: 'assets/img/asesoria-contable-tributaria.jpg',
+      image: './assets/img/asesoria-contable-tributaria.jpg',
       title: 'Asesorías Contables y tributarias.',
       description: 'Servicios de asesorías contables, orientados a optimizar la gestión financiera y administrativa de tu empresa. Afiliaciones a la seguridad social, nómina, entre otros.',
       url: CONST.waLinkAsesoriaContableTributaria
     },
     {
-      image: 'assets/img/aseo-limpieza.jpg',
+      image: './assets/img/aseo-limpieza.jpg',
       title: 'Servicios de Limpieza',
       description: 'Nuestro equipo esta disponible para ti el tiempo que tu lo requieras, medio tiempo, 6 horas o tiempo completo, con la mejor actitud y amor por lo que hacen.',
       url: CONST.waLinkServiciosDeLimpieza
     },
     {
-      image: 'assets/img/reparacion-mantenimiento-acabados.jpg',
+      image: './assets/img/reparacion-mantenimiento-acabados.jpg',
       title: 'Reparación, mantenimiento y acabados.',
       description: 'Soluciones integrales para garantizar la operatividad y conservación de tus instalaciones.',
       url: CONST.waLinkReparacionMantenimientoAcabados
